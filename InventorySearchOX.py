a549cd689f41a3b6831cf432ace057abf51141c5
jamazzz <114000467+jamazzz@users.noreply.github.com>
2024-05-29 16:31:38 +0100

    Update InventorySearchOX.py

--- InventorySearchOX.py
+++ InventorySearchOX.py
@@ -5,14 +5,16 @@ import shutil
 ######### CONFIG ##########
 
 inventoryPath = 'C:/Users/Duart/Desktop/ox_inventory' # Path to the inventory folder (Needs to use / instead of \)
+corePath = 'C:/Users/Duart/Desktop/qb-core' # QB ONLY # Path to the core folder (Needs to use / instead of \)
 
 #### DO NOT EDIT BELOW ####
 
+inventoryType = inventoryPath.split('/')[-1]
 desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
 
-itemsPath = inventoryPath+'/data/items.lua' # Path to the items folder
-weaponsPath = inventoryPath+'/data/weapons.lua' # Path to the weapons folder
-imagePath = inventoryPath+'/web/images' # Path to the current images folder
+itemsPath = inventoryPath+'/data/items.lua' if inventoryType == 'ox_inventory' else corePath+'/shared/items.lua' # Path to the items folder
+weaponsPath = inventoryPath+'/data/weapons.lua' if inventoryType == 'ox_inventory' else corePath+'/shared/weapons.lua' # Path to the weapons folder
+imagePath = inventoryPath+'/web/images' if inventoryType == 'ox_inventory' else inventoryPath+'/html/images' # Path to the images folder
 missingPath = desktop+'/missing.txt' # Path to the missing txt file
 extrasPath = desktop+'/extras' # Path to the new extra images folder
 
@@ -30,43 +32,60 @@ if not os.path.exists(missingPath):
     with open(missingPath, 'w') as f:
         pass
 
-# Images
-for _, _, files in os.walk(imagePath):
-    for file in files:
-      filename = os.path.splitext(file)[0]
-      image_list.append(filename)
+if inventoryType == 'ox_inventory':
+    # Images
+    for _, _, files in os.walk(imagePath):
+        for file in files:
+          filename = os.path.splitext(file)[0]
+          image_list.append(filename)
 
-# Items
+    # Items
+        with open(itemsPath, 'r') as file:
+            content = file.read()
+
+        double_quote_pattern = r'\["(.*?)"\]'
+        single_quote_pattern = r"\['(.*?)'\]"
+
+        double_quoted_texts = re.findall(double_quote_pattern, content)
+        single_quoted_texts = re.findall(single_quote_pattern, content)
+        for text in double_quoted_texts:
+            item_list.append(text)
+
+        for text in single_quoted_texts:
+            item_list.append(text)
+
+
+    # Weapons
+
+        with open(weaponsPath, 'r') as file:
+            content = file.read()
+
+        double_quote_pattern = r'\["(.*?)"\]'
+        single_quote_pattern = r"\['(.*?)'\]"
+
+        double_quoted_texts = re.findall(double_quote_pattern, content)
+        single_quoted_texts = re.findall(single_quote_pattern, content)
+        for text in double_quoted_texts:
+            weapon_list.append(text)
+
+        for text in single_quoted_texts:
+            weapon_list.append(text)
+else:
+    # Images
+    for _, _, files in os.walk(imagePath):
+        for file in files:
+          filename = os.path.splitext(file)[0]
+          image_list.append(filename)
+
+    # Items
+    count = 0
     with open(itemsPath, 'r') as file:
-        content = file.read()
-
-    double_quote_pattern = r'\["(.*?)"\]'
-    single_quote_pattern = r"\['(.*?)'\]"
-    
-    double_quoted_texts = re.findall(double_quote_pattern, content)
-    single_quoted_texts = re.findall(single_quote_pattern, content)
-    for text in double_quoted_texts:
-        item_list.append(text)
-
-    for text in single_quoted_texts:
-        item_list.append(text)
-
-
-# Weapons
-
-    with open(weaponsPath, 'r') as file:
-        content = file.read()
-
-    double_quote_pattern = r'\["(.*?)"\]'
-    single_quote_pattern = r"\['(.*?)'\]"
-    
-    double_quoted_texts = re.findall(double_quote_pattern, content)
-    single_quoted_texts = re.findall(single_quote_pattern, content)
-    for text in double_quoted_texts:
-        weapon_list.append(text)
-
-    for text in single_quoted_texts:
-        weapon_list.append(text)
+        for line in file:
+            count += 1
+            line = line.strip()
+            if line and not line.startswith('-') and count >2: 
+                item = line.split()[0]  
+                item_list.append(item)
 
 # Check for missing images
 
@@ -80,6 +99,7 @@ for item in item_list and weapon_list:
 
 for image in image_list:
   if image not in item_list and image not in weapon_list:
-    print(image)
     shutil.copy(imagePath + '/' + image + '.png', extrasPath + '/' + image + '.png')
     os.remove(imagePath + '/' + image + '.png')
+
+print('Done!')
