72cceb95a86e4314e3b26b709122293a24386c9d
jamazzz <114000467+jamazzz@users.noreply.github.com>
2024-05-29 22:21:29 +0100

    Update InventorySearch.py

--- InventorySearch.py
+++ InventorySearch.py
@@ -27,6 +27,7 @@ weapon_list = []
 
 if not os.path.exists(extrasPath):
     os.makedirs(extrasPath)
+    
 
 if not os.path.exists(missingPath):
     with open(missingPath, 'w') as f:
@@ -36,8 +37,7 @@ if inventoryType == 'ox_inventory':
     # Images
     for _, _, files in os.walk(imagePath):
         for file in files:
-          filename = os.path.splitext(file)[0]
-          image_list.append(filename)
+          image_list.append(file)
 
     # Items
         with open(itemsPath, 'r') as file:
@@ -74,19 +74,14 @@ else:
     # Images
     for _, _, files in os.walk(imagePath):
         for file in files:
-          filename = os.path.splitext(file)[0]
-          image_list.append(filename)
+            image_list.append(file)
 
     # Items
     count = 0
     with open(itemsPath, 'r') as file:
         for line in file:
-            count += 1
-            line = line.strip()
-            if line and not line.startswith('-') and count >2: 
-                item = line.split()[0]  
-                item_list.append(item)
-
+            if 'image' in line:
+                    item_list.append(re.search(r"['\"](.*?)['\"]", line).group(1))
 # Check for missing images
 
 open(missingPath, 'w').close()
@@ -94,12 +89,13 @@ for item in item_list and weapon_list:
     if item not in image_list:
         with open(missingPath, 'a') as file:
             file.write(item + '\n')
+            print(f'Missing {item}')
 
 # Move the extra images to the new folder
 
 for image in image_list:
-  if image not in item_list and image not in weapon_list:
-    shutil.copy(imagePath + '/' + image + '.png', extrasPath + '/' + image + '.png')
-    os.remove(imagePath + '/' + image + '.png')
+    if image.split('.')[0] not in item_list and image.split('.')[0] not in weapon_list:
+        shutil.move(imagePath+'/'+image, extrasPath+'/'+image)
+        print(f'Moved {image} to {extrasPath}')
 
 print('Done!')
